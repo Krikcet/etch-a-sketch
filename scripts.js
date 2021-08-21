c9c34d68fe7950d738b32402e26a9d50bf53ae53
Krikcet <jakelogue84@gmail.com>
2021-08-21 11:43:40 -0400

    newGrid button will remove old grid before creating new grid

--- scripts.js
+++ scripts.js
@@ -6,11 +6,24 @@ create element("div");
 append grid divs to container div;
 
 */
+document.getElementById('newGridBtn').addEventListener('click', newGrid);
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-let gridRoot = prompt("How many squares?", "256, 1024, 4096, 10000");
+function newGrid() {
+removeAllChildNodes(container);
+let gridRoot = prompt("Square root of grid?", "16, 32, 48, 100");
 divQuantity = Math.pow(gridRoot, 2);
 for(i = 0; i < divQuantity; i++) {
+    if (gridRoot > 100) {
+        newGrid();
+    } else {
    let grid = document.createElement("div");
     container.appendChild(grid);
-    grid.setAttribute('id', "grid");
+    grid.setAttribute('class', "grid",);
+    }
+}
 }
