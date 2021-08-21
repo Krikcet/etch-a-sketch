55ad4de1e8f1bf88ec10b7cef666663c18f86da7
Krikcet <jakelogue84@gmail.com>
2021-08-21 10:53:47 -0400

    set divQuantity to the square of user input

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 /*
 Loop to create divs;
-i = 0; loop while i <= divAmount; add a div on each loop;
+i = 0; loop while i <= divQuantity; add a div on each loop;
 create element("div");
+;
 append grid divs to container div;
+
 */
 
-let divAmount = prompt("How many squares?", "256, 1024, 4096, 10000");
-for(i = 0; i < divAmount; i++) {
+let gridRoot = prompt("How many squares?", "256, 1024, 4096, 10000");
+divQuantity = Math.pow(gridRoot, 2);
+for(i = 0; i < divQuantity; i++) {
    let grid = document.createElement("div");
     container.appendChild(grid);
     grid.setAttribute('id', "grid");
