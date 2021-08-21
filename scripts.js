03e8313fe0d08cc9eef497b78ea199b4406c3d7f
Krikcet <jakelogue84@gmail.com>
2021-08-20 20:01:05 -0400

    style grid

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ i = 0; loop while i <= divAmount; add a div on each loop;
 create element("div");
 append grid divs to container div;
 */
+
 let divAmount = prompt("How many squares?");
 for(i = 0; i <= divAmount; i++) {
    let grid = document.createElement("div");
